c83859dcf044d1fe2d8c56b31da89de30bb94e3d
Nayara Fonseca <88727450+NayFonseca@users.noreply.github.com>
2022-08-30 09:07:20 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+window.alert("Olá Mundo !");
+window.confirm("Tem certeza que deseja aprender JavaScript ?");
+window.prompt("Por favor diga seu nome","Seu nome aqui.");
+window.alert("Olá " + window.prompt("Por favor diga seu nome","Seu nome aqui."));
+window.console.error("Ocorreu um erro.");
+window.console.warn("Isto é somente um alerta !");
+window.console.log("Isto é apenas uma mensagem.")
